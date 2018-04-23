b28de3b1f408e913665858107f29c3400855f8ff
Alex Reardon <areardon@atlassian.com>
2018-04-23 10:36:50 +1000

    streamlining

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Box model 📦
+# CSS box model 📦
 
 > Get the [CSS box model](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Box_Model/Introduction_to_the_CSS_box_model) from a [`HTMLElement`](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement)
 
--- src/index.js
+++ src/index.js
@@ -24,49 +24,50 @@
 
 export type BoxModel = {|
   // content + padding + border + margin
-  marginBox: Area,
+  marginBox: DOMRect,
   // content + padding + border
-  borderBox: Area,
+  borderBox: DOMRect,
   // content + padding
-  paddingBox: Area,
+  paddingBox: DOMRect,
   // content
-  contentBox: Area,
+  contentBox: DOMRect,
+  // for your own consumption
+  border: Spacing,
+  padding: Spacing,
+  margin: Spacing,
 |};
 
-type Spacing = {
+export type AnyRectType = ClientRect | DOMRect;
+
+export type Spacing = {
   top: number,
   right: number,
   bottom: number,
   left: number,
 };
 
-export type Position = {|
-  x: number,
-  y: number,
-|};
+const getRect = ({ top, right, bottom, left }: Spacing): DOMRect => {
+  const width: number = right - left;
+  const height: number = bottom - top;
 
-export type Area = {|
-  top: number,
-  right: number,
-  bottom: number,
-  left: number,
-  width: number,
-  height: number,
-  center: Position,
-|};
+  // TODO: IE and edge do not support DOMRect (they support the non-standard ClientRect)
+  if (typeof DOMRect !== 'undefined') {
+    return new DOMRect(top, left, width, height);
+  }
 
-const getArea = ({ top, right, bottom, left }: Spacing): Area => ({
-  top,
-  right,
-  bottom,
-  left,
-  width: right - left,
-  height: bottom - top,
-  center: {
-    x: (right + left) / 2,
-    y: (bottom + top) / 2,
-  },
-});
+  const fake: DOMRect = {
+    top,
+    right,
+    bottom,
+    left,
+    width,
+    height,
+    x: top,
+    y: left,
+  };
+
+  return fake;
+};
 
 const expand = (target: Spacing, expandBy: Spacing): Spacing => ({
   // pulling back to increase size
@@ -86,39 +87,60 @@ const shrink = (target: Spacing, shrinkBy: Spacing): Spacing => ({
   right: target.right - shrinkBy.right,
 });
 
-export default (el: Element): BoxModel => {
-  // getBoundingClientRect always returns the borderBox
-  const rect: ClientRect = el.getBoundingClientRect();
-  const styles: Object = window.getComputedStyle(el);
+const parse = (value: string): number => parseInt(value, 10);
 
+// export const getCenter = (rect: AnyRectType): Position => {};
+
+// export const withScroll = (
+//   rect: AnyRectType,
+//   scroll?: Position = getWindowScroll(),
+// ): DOMRect => {};
+
+// Exposing this function directly for performance. If you have already computed these things
+// then you can simply pass them in
+export const calculateBox = (
+  rect: DOMRect | ClientRect,
+  styles: CSSStyleDeclaration,
+): BoxModel => {
   const margin: Spacing = {
-    top: parseInt(styles.marginTop, 10),
-    right: parseInt(styles.marginRight, 10),
-    bottom: parseInt(styles.marginBottom, 10),
-    left: parseInt(styles.marginLeft, 10),
+    top: parse(styles.marginTop),
+    right: parse(styles.marginRight),
+    bottom: parse(styles.marginBottom),
+    left: parse(styles.marginLeft),
   };
   const padding: Spacing = {
-    top: parseInt(styles.paddingTop, 10),
-    right: parseInt(styles.paddingRight, 10),
-    bottom: parseInt(styles.paddingBottom, 10),
-    left: parseInt(styles.paddingLeft, 10),
+    top: parse(styles.paddingTop),
+    right: parse(styles.paddingRight),
+    bottom: parse(styles.paddingBottom),
+    left: parse(styles.paddingLeft),
   };
   const border: Spacing = {
-    top: parseInt(styles.borderTopWidth, 10),
-    right: parseInt(styles.borderRightWidth, 10),
-    bottom: parseInt(styles.borderBottomWidth, 10),
-    left: parseInt(styles.borderLeftWidth, 10),
+    top: parse(styles.borderTopWidth),
+    right: parse(styles.borderRightWidth),
+    bottom: parse(styles.borderBottomWidth),
+    left: parse(styles.borderLeftWidth),
   };
 
-  const borderBox: Area = getArea(rect);
-  const marginBox: Area = getArea(expand(borderBox, margin));
-  const paddingBox: Area = getArea(shrink(borderBox, border));
-  const contentBox: Area = getArea(shrink(paddingBox, padding));
+  const borderBox: DOMRect = getRect(rect);
+  const marginBox: DOMRect = getRect(expand(borderBox, margin));
+  const paddingBox: DOMRect = getRect(shrink(borderBox, border));
+  const contentBox: DOMRect = getRect(shrink(paddingBox, padding));
 
   return {
     marginBox,
     borderBox,
     paddingBox,
     contentBox,
+    border,
+    padding,
+    margin,
   };
+};
+
+export default (el: Element): BoxModel => {
+  // getBoundingClientRect always returns the borderBox
+  const rect: ClientRect = el.getBoundingClientRect();
+  const styles: Object = window.getComputedStyle(el);
+
+  return calculateBox(rect, styles);
 };
