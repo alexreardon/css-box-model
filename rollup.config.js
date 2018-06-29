f940e0aa9de32ddf54434cdbeefdd2104a90ed0d
Alex Reardon <areardon@atlassian.com>
2018-06-29 13:59:53 +1000

    more bundle options

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
+// @flow
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import { uglify } from 'rollup-plugin-uglify';
 
 const input = 'src/index.js';
 
@@ -20,5 +24,36 @@ export default [
       format: 'cjs',
     },
     plugins: [babel()],
+  },
+  // Universal module definition (UMD) build
+  {
+    input,
+    output: {
+      file: 'dist/css-box-model.js',
+      format: 'umd',
+      name: 'cssBoxModel',
+    },
+    plugins: [
+      // Setting development env before running babel etc
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      babel(),
+      commonjs({ include: 'node_modules/**' }),
+    ],
+  },
+  // Universal module definition (UMD) build (production)
+  {
+    input,
+    output: {
+      file: 'dist/css-box-model.min.js',
+      format: 'umd',
+      name: 'cssBoxModel',
+    },
+    plugins: [
+      // Setting production env before running babel etc
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      babel(),
+      commonjs({ include: 'node_modules/**' }),
+      uglify(),
+    ],
   },
 ];
